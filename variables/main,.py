1ce3a9a33f09e1a6a72433c60b2b12c88f0f4fe2
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-05-09 10:16:25 -0500

    Primera clase de python

---
+++ variables/main,.py
@@ -0,0 +1,39 @@
+## es una característica, cualidad o propiedad observada que puede adquirir diferentes valores,
+## donde podemos almacenar datos
+nombre="edwin"
+## nombre -> es el identificador del
+## espacio de memoria que vamos a usar
+
+## = -> es el signo de asignacion que nos indica que vamos a almacenar
+
+## edwin -> es el dato que estamos almacenando
+
+## el identificador (nombre) como apunta aun espacio de memoria no se puede cambiar o editar.
+## sin embargo el dato que almacena puede mutar en cualquier momento
+
+## pasos para declarar una variable
+## 1 . darle un nombre al identificador(variable)
+## 2 . escribir el operador de adignacion(=)
+## 3 . escribir el dato a almacenar
+
+## Reglas para  nombrar variables
+## 1. solo pueden tener los siguientes caracteres
+##      - Letras minusculas
+##      - Letras mayusculas
+##      - Digitos
+##      - Guiones bajos (_)
+## 2. solo deben iniciar con letras y guion bajo no con digitos
+## 3. no pueden ser una palabra reservada del lenguaje
+
+## ejemplos de variable validas
+
+a="valido"      
+a3="valido"
+a_b_C="valido"
+_abc="valido"
+_3a="valido"
+3="invalido"
+3a="invalido"
+mi-variable="invalido"
+with="invalido"
+3_a="invalido"
