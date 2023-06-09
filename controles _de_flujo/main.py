43e930bcb1f8061a3685f2b4782f8979afb031b9
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-06-09 09:47:38 -0500

    clase controles de flujo

---
+++ controles _de_flujo/main.py
@@ -0,0 +1,26 @@
+
+##bucles
+condicion=0
+while condicion<11:
+    print(condicion)
+    condicion=condicion+1
+
+condicion1=0
+while condicion1<21:
+    print(condicion1)
+    condicion1=condicion1+2
+
+edad=0
+while edad!=20:
+    edad=int(input("Ingrese su edad: "))
+
+nombre=""
+while nombre!="si":
+    nombre=input("Ingrese su nombre: ")
+
+while True:
+    nombre1=input("como te llamas: ")
+    if nombre1=="si":
+        break
+
+
