8d7c1d0b314a7556532a6afceb687046ce72e583
AstaGod <bautistapomacrhistianalexis@gmail.com>
2023-06-22 12:27:37 -0500

    clase funcion for

--- controles _de_flujo/main.py
+++ controles _de_flujo/main.py
@@ -1,26 +1,54 @@
 
 ##bucles
-condicion=0
-while condicion<11:
-    print(condicion)
-    condicion=condicion+1
+#condicion=0
+#while condicion<11:
+    #print(condicion)
+    #condicion=condicion+1
 
-condicion1=0
-while condicion1<21:
-    print(condicion1)
-    condicion1=condicion1+2
+#condicion1=0
+#while condicion1<21:
+    #print(condicion1)
+    #condicion1=condicion1+2
 
-edad=0
-while edad!=20:
-    edad=int(input("Ingrese su edad: "))
+#edad=0
+#while edad!=20:
+    #edad=int(input("Ingrese su edad: "))
 
-nombre=""
-while nombre!="si":
-    nombre=input("Ingrese su nombre: ")
+#nombre=""
+#while nombre!="si":
+    #nombre=input("Ingrese su nombre: ")
 
-while True:
-    nombre1=input("como te llamas: ")
-    if nombre1=="si":
+#while True:
+    #nombre1=input("como te llamas: ")
+    #if nombre1=="si":
+       # break
+
+
+for numero in range(1,21):
+    print(numero)
+
+vocales=["a","e","i","o","u"]
+for numero in range(0,5):
+    print(vocales[numero])
+
+for vocal in vocales:
+    print(vocal)
+
+## CREAR UNA LISTA DE CINCO COLORES CADA COLOR QUE INTERES
+## TENDRAS QUE MOSTRAR POR CONSOLA SOLO CUANDO ENCUENTRE EL 
+## COLOR ROJO MOSTRARA EL MENSAJE DE
+## COLO ENCONTRADO Y SE TERMINARA LA EJECUCION
+
+colores=["negro","azul","rosa","rojo","amarillo"]
+for color in colores:
+    print(color)
+    if color == "rosa":
+        print("color encontrado")
         break
-
-
+    
+colores=["negro","azul","rosa","rojo","amarillo"]
+for color in colores:
+    if color == "rojo":
+     print("encontrado")
+     break
+    print(color)
